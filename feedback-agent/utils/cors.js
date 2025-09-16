8331f470f34215432931fcc78d1d050dbff3c92d
Freedomtukun <167708652+Freedomtukun@users.noreply.github.com>
2025-09-16 15:56:04 -0500

    Delete feedback-agent/utils directory

--- feedback-agent/utils/cors.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-exports.corsHeaders = () => ({
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type,Authorization'
-});
-exports.isPreflight = (method) => method === 'OPTIONS';
